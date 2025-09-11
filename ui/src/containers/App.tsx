3329f81ccd032b2625817bb59130113ab98236e5
Mateus <mpcmarques@icloud.com>
2025-09-11 12:50:47 +0100

    upgraded to typescrip swc

--- ui/src/containers/App.tsx
+++ ui/src/containers/App.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { debugData } from "../utils/debugData";
-import Home from "./Home";
 
 // This will set the NUI to visible if we are
 // developing in browser
 debugData([
-	{
-		action: "setVisible",
-		data: true,
-	},
+    {
+        action: "setVisible",
+        data: true,
+    },
 ]);
 
 const App: React.FC = () => {
-	return <div className="flex flex-col min-h-screen">FiveM Template</div>;
+    return <div className="flex flex-col min-h-screen">FiveM Template</div>;
 };
 
 export default App;
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,16 +3,11 @@ import ReactDOM from "react-dom/client";
 import { VisibilityProvider } from "./providers/VisibilityProvider";
 import App from "./containers/App";
 import "./index.css";
-import * as dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
-
-// setup Day.js
-dayjs.extend(relativeTime);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		<VisibilityProvider>
-				<App />
-		</VisibilityProvider>
-	</React.StrictMode>
+    <React.StrictMode>
+        <VisibilityProvider>
+            <App />
+        </VisibilityProvider>
+    </React.StrictMode>,
 );
